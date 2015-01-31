fe8f2b75f4f14e4997ae2b9dda2e0be9bd850e8c
Brian DeRenzi <bderenzi@gmail.com>
2015-01-31 15:38:58 +0300

    Added the liverelaod to default

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,22 @@ var less = require('gulp-less');
 var path = require('path');
 var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
+var livereload = require('gulp-livereload');
 
-gulp.task('default', function() {
+gulp.task('less', function() {
     gulp.src('./contacts/static/style.less')
         .pipe(sourcemaps.init())
         .pipe(less())
         .pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest('./contacts/static/css'));
+        .pipe(gulp.dest('./contacts/static/css'))
+        .pipe(livereload());
 });
 
+gulp.task('watch', function() {
+	livereload.listen();
+    gulp.watch('./contacts/static/**/*.less', ['less']);
+});
 
-// {
-//       paths: [ path.join(__dirname, 'less', 'includes') ]
-//     }
+gulp.task('default', ['less', 'watch'], function() {
+	return
+});
