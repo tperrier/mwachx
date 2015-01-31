9de3d7e5b32c1ecfc8b229c2848d90055a85bd72
Trevor <t.perrier@gmail.com>
2015-01-31 00:27:21 -0800

    contact list fragment filtering templated

--- contacts/static/js/contact_list.js
+++ contacts/static/js/contact_list.js
@@ -4,13 +4,19 @@ $(function(){// On DOM Load
        var code = evt.which;
        switch (code){
             case 13://enter
-                contact_list_tools.search();
+                contact_list_tools.filter();
                 break;
             case 17://ctrl
+            case 27://ctrl
                 $('#contact-search').val('');
-                contact_list_tools.search();
+                contact_list_tools.filter();
                 break;
         }
+    });
+    
+    $('.filter-icons li').click(function(evt){
+        $(this).toggleClass('active');
+        contact_list_tools.filter();
     });
     
     $('#contact-search-icon').click(contact_list_tools.search);
@@ -20,13 +26,33 @@ $(function(){// On DOM Load
 
 var contact_list_tools = { // name space for contact list functions
     
-    search:function(){
+    filter:function(){
         console.log('Search...');
-        needle = new RegExp($('#contact-search').val(),'i') //case insensitive regex ;
+        needle_regex_str = $('#contact-search').val() || '.*';
+        var needle = new RegExp(needle_regex_str,'i') //case insensitive regex ;
+        
+        var group_filter = function(ele) {
+            value = 
+                ( $('#two-way-filter').hasClass('active') && ele.data('group') == 'two-way')
+                || ($('#one-way-filter').hasClass('active') && ele.data('group') == 'one-way')
+                || ($('#control-filter').hasClass('active') && ele.data('group') == 'control');
+            return value;
+        }
+        
+        var pregnant_filter = function(ele) {
+            return $('#pregnant-filter').hasClass('active') && ele.data('pregnant') == 'True';
+        }
+        
+        var pending_filter = function(ele) {
+            var active = $('#pending-filter').hasClass('active') 
+            if(active) return ele.data('pending') > 0;
+            return true;
+        }
+        
         $('#contact-list .contact').each(function(){
             $ele = $(this);
-            console.log($ele);
-           if(needle.test($ele.data('searchStr'))) {
+            console.log(pregnant_filter($ele));
+           if(needle.test($ele.data('searchStr')) && group_filter($ele) && pending_filter($ele) ){
                $ele.show();
            }else {
                $ele.hide();
