1afef23b5a2971e3aa27aa06c73aa7673239ce8e
Brian DeRenzi <bderenzi@gmail.com>
2015-02-02 11:53:21 +0300

    replaced fieldset with a header for better semantics

--- utils/context_processors.py
+++ utils/context_processors.py
@@ -1,8 +1,12 @@
 from django.conf import settings
+import datetime
 
 def openshift(context):
-  return {'ON_OPENSHIFT': settings.ON_OPENSHIFT}
+	return {'ON_OPENSHIFT': settings.ON_OPENSHIFT}
 
 def current_date(context):
-  return {'CURRENT_DATE': settings.CURRENT_DATE}
+	return {
+		'CURRENT_DATE': settings.CURRENT_DATE,
+		'ONE_WEEK': settings.CURRENT_DATE + datetime.timedelta(weeks=1)
+	}
 
