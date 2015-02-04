31ffe2e6f0485e2f6e1f9a1672bffc22791b70c4
Trevor <t.perrier@gmail.com>
2015-02-03 21:28:05 -0800

    Closed #36 added basic search featurs for contacts

--- contacts/static/js/contact_list.js
+++ contacts/static/js/contact_list.js
@@ -27,31 +27,34 @@ $(function(){// On DOM Load
 var contact_list_tools = { // name space for contact list functions
     
     filter:function(){
-        console.log('Search...');
         needle_regex_str = $('#contact-search').val() || '.*';
         var needle = new RegExp(needle_regex_str,'i') //case insensitive regex ;
+        console.log('Search...',needle_regex_str);
         
         var group_filter = function(ele) {
             value = 
                 ( $('#two-way-filter').hasClass('active') && ele.data('group') == 'two-way')
                 || ($('#one-way-filter').hasClass('active') && ele.data('group') == 'one-way')
                 || ($('#control-filter').hasClass('active') && ele.data('group') == 'control');
+            return true;
             return value;
         }
         
         var pregnant_filter = function(ele) {
+            return true;
             return $('#pregnant-filter').hasClass('active') && ele.data('pregnant') == 'True';
         }
         
         var pending_filter = function(ele) {
+            return true;
             var active = $('#pending-filter').hasClass('active') 
             if(active) return ele.data('pending') > 0;
             return true;
         }
         
-        $('#contact-list .contact').each(function(){
+        $('#contacts-table .contact').each(function(){
             $ele = $(this);
-            console.log(pregnant_filter($ele));
+            //console.log(pregnant_filter($ele));
            if(needle.test($ele.data('searchStr')) && group_filter($ele) && pending_filter($ele) ){
                $ele.show();
            }else {
