2486d98ea0a0a0f02e6b2814caec1a955783f4e5
Brian DeRenzi <bderenzi@gmail.com>
2015-02-09 17:29:15 +0300

    closed #55, closed #54. that took longer than expected

--- contacts/forms.py
+++ contacts/forms.py
@@ -1,6 +1,7 @@
 #Django Imports
 from django import forms
 
+
 #Local App Imports
 import contacts.models as cont
 import utils.forms as util
@@ -14,9 +15,9 @@ class ContactAdd(forms.ModelForm):
         exclude = ['status','child_hiv_status']
         
         widgets = {
-            'due_date':util.Html5DateInput(),
-            'birthdate':util.Html5DateInput(),
-            'art_initiation':util.Html5DateInput(),
+            'due_date': util.FuelDatePicker('due_date'),
+            'birthdate': util.FuelDatePicker('birthdate', allow_past=True),
+            'art_initiation': util.FuelDatePicker('art_initiation', allow_past=True),
         }
 
 class ContactModify(forms.ModelForm):
---
+++ contacts/templatetags/css_hack.py
@@ -0,0 +1,10 @@
+from django import template
+register = template.Library()
+
+@register.filter(name='addcss')
+def addcss(field, css):
+   return field.as_widget(attrs={"class":css})
+
+@register.filter(name='addcss_label')
+def addcss_label(field, css):
+   return field.label_tag(attrs={"class":css})
