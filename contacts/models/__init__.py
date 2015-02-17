0c242da71eaea7a44c83d58f425d07c1d4ab3184
Trevor <t.perrier@gmail.com>
2015-02-16 23:37:25 -0800

    the __all__ variable wasnt needed

--- contacts/models/__init__.py
+++ contacts/models/__init__.py
@@ -5,7 +5,5 @@ from contacts.models.visit import Visit
 from contacts.models.contact import Contact
 from contacts.models.misc import PhoneCall, Note, Connection,StatusChange,Facility,Practitioner
 
-__all__  = ['Contact','Message','Language','Translation','Visit','PhoneCall',
-            'Note','Connection','StatusChange','Facility','Practitioner']
 
 
