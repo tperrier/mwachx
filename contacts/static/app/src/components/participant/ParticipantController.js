25dfea1852478d22a606ba86005c0373042100ef
Brian DeRenzi <bderenzi@gmail.com>
2015-03-04 18:01:02 +0300

    Prettied up the messages
    No working buttons, no message headers or footers, but it is starting
    to look reasonable.

--- contacts/static/app/src/components/participant/ParticipantController.js
+++ contacts/static/app/src/components/participant/ParticipantController.js
@@ -27,7 +27,16 @@
         // Fetch messages for this participant
         Message.query( {study_id:$routeParams.study_id},
           function(response) {
+            
             $scope.messages = response;
+            for (var i = $scope.messages.length - 1; i >= 0; i--) {
+              if ($scope.messages[i].is_system == false && $scope.messages[i].is_outgoing == true) {
+                $scope.messages[i].is_nurse = true;
+              } else {
+                $scope.messages[i].is_nurse = false;
+              }
+
+            };
         });
 
       });
