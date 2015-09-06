209301dfbeb14db957404d93e10f6d3f6622d3c8
Trevor <t.perrier@gmail.com>
2015-09-06 15:56:05 -0700

    feat(gulp) add js concatenation

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var path = require('path');
 var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
 var livereload = require('gulp-livereload');
+var concat = require('gulp-concat')
 
 gulp.task('less', function() {
     gulp.src('./mwach/static/less/main.less')
@@ -12,6 +13,15 @@ gulp.task('less', function() {
         .pipe(gulp.dest('./mwach/static/css'))
         .pipe(livereload());
 });
+
+// From: https://medium.com/@dickeyxxx/best-practices-for-building-angular-js-apps-266c1a4a6917
+gulp.task('js',function () {
+  gulp.src(['./mwach/static/app/mwachx.module.js','mwach/static/app/**/*.js'])
+    .pipe(sourcemaps.init())
+      .pipe(concat('mwachx.js'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./mwach/static'))
+})
 
 gulp.task('watch', function() {
 	livereload.listen();
