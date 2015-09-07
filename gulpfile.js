bfcc35adb69ce6b4b83a51f169cbd46873cb0bed
Trevor <t.perrier@gmail.com>
2015-09-06 18:35:09 -0700

    feat(index) concat bower_componets into components.js
    Currently to add more compoentents you need to modify gulpfile.js
    There is probably a way to load the list of compenents from a file.

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,8 @@ var path = require('path');
 var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
 var livereload = require('gulp-livereload');
-var concat = require('gulp-concat')
-var uglify = require('gulp-uglify')
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 gulp.task('less', function() {
     gulp.src('./mwach/static/less/main.less')
@@ -23,7 +23,21 @@ gulp.task('js',function () {
       // .pipe(uglify())
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./mwach/static'))
-})
+});
+
+gulp.task('libs',function(){
+  gulp.src([
+    'mwach/static/bower_components/angular/angular.js',
+    'mwach/static/bower_components/angular-ui-router/release/angular-ui-router.js',
+    'mwach/static/bower_components/angular-resource/angular-resource.js',
+    'mwach/static/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+    'mwach/static/bower_components/angular-bootstrap-show-errors/src/showErrors.js'
+  ])
+   .pipe(sourcemaps.init())
+    .pipe(concat('components.js'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./mwach/static'))
+});
 
 gulp.task('watch', function() {
 	livereload.listen();
