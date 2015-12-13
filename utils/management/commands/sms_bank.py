448170c12259067c8d805fbbdf9810d665fd47dc
Trevor <t.perrier@gmail.com>
2015-12-12 18:30:42 -0800

    feat(sms_bank) major improvements to sms_bank work flow

--- utils/management/commands/sms_bank.py
+++ utils/management/commands/sms_bank.py
@@ -12,33 +12,21 @@ class Command(BaseCommand):
 
     help = 'Parse and import SMS data bank'
 
-    class Paths:
-        base = 'translations'
-        bank = os.path.join(base,'bank.xlsx')
-
-        todo_swahili = os.path.join(base,'todo_swahili.xlsx')
-        todo_luo = os.path.join(base,'todo_luo.xlsx')
-
-        done_swahili = os.path.join(base,'done_swahili.xlsx')
-        done_luo = os.path.join(base,'done_luo.xlsx')
-
-        final = os.path.join(base,'translations.xlsx')
-
     def add_arguments(self,parser):
         # code.interact(local=locals())
+        parser.add_argument('-d','--dir',help='directory to find translations in default (translations)',default='translations')
         subparsers = parser.add_subparsers(help='sms bank commands')
 
         # The cmd argument is required for django.core.management.base.CommandParser
-        check_parser = subparsers.add_parser('check',cmd=parser.cmd,help='print info for sms bank or translations')
-        check_parser.add_argument('type',type=bank_or_translation,help='type of check. one of (b)ank or (t)anslation')
-        check_parser.set_defaults(action='check_messages')
-
         make_parser = subparsers.add_parser('make',cmd=parser.cmd,help='make final translations or todos')
         make_parser.add_argument('type',type=todo_or_final,help='type of make. one of (t)do or (f)inal')
+        make_parser.add_argument('-c','--copy',help='make copy of todo messages',action='store_true',default=False)
         make_parser.set_defaults(action='make_messages')
 
-        clean_parser = subparsers.add_parser('clean',cmd=parser.cmd,help='clean messages bank or translations')
-        clean_parser.add_argument('type',type=bank_or_translation,help='type of check. one of (b)ank or (t)anslation')
+        check_parser = subparsers.add_parser('check',cmd=parser.cmd,help='check and print stats for final translations')
+        check_parser.set_defaults(action='check_messages')
+
+        clean_parser = subparsers.add_parser('clean',cmd=parser.cmd,help='clean messages bank')
         clean_parser.set_defaults(action='clean_messages')
 
         import_parser = subparsers.add_parser('import',cmd=parser.cmd,help='import messages to backend')
@@ -53,6 +41,20 @@ class Command(BaseCommand):
 
     def handle(self,*args,**options):
 
+        self.stdout.write( 'Action: {} Root: {}'.format(options['action'],options['dir']) )
+
+        self.paths = argparse.Namespace(
+            bank = os.path.join(options['dir'],'bank.xlsx'),
+
+            todo_swahili = os.path.join(options['dir'],'todo_swahili.xlsx'),
+            todo_luo = os.path.join(options['dir'],'todo_luo.xlsx'),
+
+            done_swahili = os.path.join(options['dir'],'done_swahili.xlsx'),
+            done_luo = os.path.join(options['dir'],'done_luo.xlsx'),
+
+            final = os.path.join(options['dir'],'translations.xlsx')
+        )
+
         self.options = options
         getattr(self,options['action'])()
 
@@ -61,7 +63,7 @@ class Command(BaseCommand):
     ########################################
 
     def custom(self):
-        sms_bank = xl.load_workbook(self.Paths.bank)
+        sms_bank = xl.load_workbook(self.paths.bank)
         new_wb = xl.workbook.workbook()
         ws = new_wb.active
 
@@ -72,57 +74,117 @@ class Command(BaseCommand):
                 msg = sms.MessageBankRow(row)
                 ws.append(msg.get_bank_row())
 
-    def print_statistics(self):
-        sms_bank = xl.load_workbook(self.options['bank'])
-        ws = sms_bank.worksheets[0]
-
-        messages = sms.parse_messages(ws)
-        stats = recursive_dd()
-        for msg in messages:
-            base = stats[msg.send_base]
-            group = base[msg.group]
-            track = group[msg.track]
-            track.default_factory = int
-            track['yes' if msg.hiv else 'no'] += 1
-
-        for base, groups in stats.items():
-            for group, tracks in groups.items():
-                for track, hiv_messaging in tracks.items():
-                    for hiv, count in hiv_messaging.items():
-                        print "{}_{}_{}_{}: {}".format(base,group,track,hiv,count)
-
     def make_messages(self):
-        if self.options.type == 'todo':
+        self.stdout.write( 'Making Messages: {}'.format(self.options['type'].capitalize()) )
+        if self.options['type'] == 'todo':
             self.make_todo_messages()
-        elif self.options.type == 'final':
+        elif self.options['type'] == 'final':
             self.make_final_messages()
 
     def make_todo_messages(self):
+        ''' Take a formated SMS bank and create language specific todo files'''
+        sms_bank = xl.load_workbook(self.paths.bank)
+        try:
+            final_wb = xl.load_workbook(self.paths.final)
+            old_translations = sms.message_dict(final_wb.active,sms.FinalRow)
+        except IOError as e:
+            old_translations = None
 
-        sms_bank = xl.load_workbook(self.Paths.bank)
-        old_translations = sms.message_dict(final_wb.active,sms.FinalRow)
-
-        translations = read_sms_bank(sms_bank,old_translations,'anc','special')
+        translations = sms.read_sms_bank(sms_bank,old_translations,'anc','special')
 
         print "Total Found: {} Todo: {}".format( len(translations),
-            len([t for t in translations if t.status == 'todo']) )
+            len([t for t in translations if t.is_todo()]) )
 
         swahili_wb = make_language_todos(translations,'swahili')
         luo_wb = make_language_todos(translations,'luo')
 
-        swahili_wb.save(self.Paths.todo_swahili)
-        luo_wb.save(self.Paths.todo_luo)
+        swahili_wb.save(self.paths.todo_swahili)
+        luo_wb.save(self.paths.todo_luo)
+
+        if self.options['copy']:
+            # TODO: finish this
+            pass
+
+    def make_final_messages(self):
+        '''Take language specific todo files and done files and make a final translations file '''
+
+        # Load todo messages or fail
+        try:
+            todo_swahili_wb = xl.load_workbook(self.paths.todo_swahili)
+            todo_luo_wb = xl.load_workbook(self.paths.todo_luo)
+        except IOError as e:
+            self.stderr.write('Error: Swahili and or Lue todo files not found')
+            return
+
+        todo_swahili_bank = sms.message_dict(todo_swahili_wb.active,sms.TranslationRow,language_name='swahili')
+        todo_luo_bank = sms.message_dict(todo_luo_wb.active,sms.TranslationRow,language_name='luo')
+
+        # Load done messages or fail
+        try:
+            done_swahili_wb = xl.load_workbook(self.paths.done_swahili)
+            done_luo_wb = xl.load_workbook(self.paths.done_luo)
+        except IOError as e:
+            self.stderr.write('Error: Swahili and or Lue done files not found')
+            return
+        done_swahili_bank = sms.message_dict(done_swahili_wb.active,sms.TranslationRow,language_name='swahili')
+        done_luo_bank = sms.message_dict(done_luo_wb.active,sms.TranslationRow,language_name='luo')
+
+        # Load sms bank
+        sms_bank = xl.load_workbook(self.paths.bank)
+        translations = sms.read_sms_bank(sms_bank,None,'anc','special')
+
+        # Create final wb and add header
+        final_wb = xl.workbook.Workbook()
+        new_ws = final_wb.active
+        new_ws.append(sms.FinalRow.header)
+
+        for msg in translations:
+            description = msg.description()
+
+            todo_swahili , todo_luo = todo_swahili_bank[description] , todo_luo_bank[description]
+            done_swahili , done_luo = done_swahili_bank[description] , done_luo_bank[description]
+            msg.swahili , msg.luo , msg.english = done_swahili.swahili , done_luo.luo , msg.new
+
+            # Determine done status
+            is_done_swahili = not done_swahili.is_todo() or todo_swahili.swahili != done_swahili.swahili
+            is_done_luo = not done_luo.is_todo() or todo_luo.luo != done_luo.luo
+            if not done_luo.is_todo() and not done_swahili.is_todo():
+                msg.status = 'clean'
+            elif is_done_swahili and is_done_luo:
+                msg.status = 'done'
+            elif is_done_swahili:
+                msg.status = 'swahili'
+            elif is_done_luo:
+                msg.status = 'luo'
+            else:
+                msg.status = 'todo'
+
+            msg.configure_variables()
+            new_ws.append(msg.get_final_row())
+
+            # Wrap text on english, swahili, luo columns
+            last = new_ws.rows[-1]
+            for i in [6,7,8]:
+                last[i].alignment = WRAP_TEXT
+
+            new_ws.freeze_panes = 'A2'
+            new_ws.auto_filter.ref = 'A1:I1'
+
+            column_widths = {'A':4,'D':6,'E':6,'F':6,'G':50,'H':50,'I':50}
+            for col_letter, width in column_widths.items():
+                new_ws.column_dimensions[col_letter].width = width
+
+        self.stdout.write('{} translations. {} todo.'.format( len(translations),
+            len([m for m in translations if m.is_todo()])
+        ) )
+
+        final_wb.save(self.paths.final)
+
 
     def check_messages(self):
 
-        if self.options['type'] == 'bank':
-            sms_wb = xl.load_workbook(self.Paths.bank)
-            messages = sms.read_sms_bank(sms_wb,None,'anc','special')
-        elif self.options['type'] == 'translation':
-            sms_wb = xl.load_workbook(self.Paths.final)
-            messages = sms.parse_messages(sms_wb.active,sms.FinalRow)
-        else:
-            raise argparse.ArgumentTypeError('invalild type')
+        sms_wb = xl.load_workbook(self.paths.final)
+        messages = sms.parse_messages(sms_wb.active,sms.FinalRow)
 
         stats = recursive_dd()
         for msg in messages:
@@ -136,57 +198,21 @@ class Command(BaseCommand):
             self.stdout.write( '{}'.format(condition_hiv) )
             for base_group,items in base_groups.items():
                 count = len(items)
-                todo = len(filter(lambda m: m.status == 'todo',items))
-                self.stdout.write('\t{} {} ({})'.format(base_group,count,todo))
-
-        # for base, groups in stats.items():
-        #     for group, tracks in groups.items():
-        #         for track, hiv_messaging in tracks.items():
-        #             for hiv, count in hiv_messaging.items():
-        #                 print "{}_{}_{}_{}: {}".format(base,group,track,hiv,count)
+                todo = len(filter(lambda m: m.is_todo(),items))
+                self.stdout.write( '\t{}: {}({}) {}'.format(
+                    base_group,count,todo,
+                    sorted([i.offset for i in items])
+                ) )
 
         # self.options['ascii_msg'] = 'Warning: non-ascii chars found: {count}'
         # non_ascii_dict = self.non_ascii_count()
         #
-        # todo = argparse.Namespace(total=0,swahili=0,luo=0)
-        # for row in sms_bank.active.rows[1:]:
-        #     msg = sms.MessageRow(row,translation=True)
-        #     if msg.status == 'todo':
-        #         todo.total += 1
-        #         todo.swahili += 1 if msg.swahili == '' else 0
-        #         todo.luo += 1 if msg.luo == '' else 0
-        #
-        # print 'Check Complete: ToDo: {0.total} Swahili:{0.swahili} Luo: {0.luo} Diff: {1}'.format(todo,todo.total-todo.swahili-todo.luo)
 
-    def process_messages(self):
-        sms_bank = xl.load_workbook(self.options['bank'])
-        old_translations = sms.message_dict(sms_bank.worksheets[0],translation=True)
-
-        translations = xl.workbook.Workbook()
-        new_ws = translations.active
-        new_ws.append(sms.MessageRow.translation_header())
-
-        todo_count = 0
-        for row in sms_bank.active.rows[1:]:
-            msg = sms.MessageRow(row,translation=True)
-            if msg.status == 'done':
-                msg.status = 'clean'
-            elif msg.status == 'todo' and msg.new != '':
-                msg.english = msg.new
-            todo_count += msg.add_translation(new_ws,old_translations)
-
-        print 'Todo:',todo_count
-        new_ws.freeze_panes = 'A2'
-        new_ws.auto_filter.ref = 'A1:K1'
-
-        column_widths = {'A':4,'D':6,'E':6,'F':6,'G':45,'H':45,'I':45,'J':45}
-        for col_letter, width in column_widths.items():
-            new_ws.column_dimensions[col_letter].width = width
-
-        translations.save('ignore/translations.xlsx')
+    def clean_messages(self):
+        self.stderr.write('Clean function not implemented!')
 
     def import_messages(self):
-        sms_bank = xl.load_workbook(self.Paths.final)
+        sms_bank = xl.load_workbook(self.paths.final)
         do_all = not self.options.get('done',False)
 
         todo,total = 0,0
@@ -195,7 +221,7 @@ class Command(BaseCommand):
             total += 1
             if do_all or msg.status == 'done':
                 auto = back.AutomatedMessage.objects.from_excel(msg)
-                if msg.status == 'todo':
+                if msg.is_todo():
                     self.stdout.write('Warning: message {} still todo'.format(msg.description()))
                     todo += 1
         self.stdout.write('Messages Imported: {} Messages Todo: {}'.format(total,todo))
@@ -221,12 +247,10 @@ class Command(BaseCommand):
 
     def test(self):
 
-        final_wb = xl.load_workbook(self.Paths.final)
+        final_wb = xl.load_workbook(self.paths.final)
         old_translations = sms.message_dict(final_wb.active,sms.FinalRow)
 
         print 'Count: {} Example: {!r}'.format(len(old_translations),old_translations.get(old_translations.keys()[0]))
-
-
 
 ########################################
 #  Utility Functions
@@ -247,7 +271,7 @@ def bank_or_translation(option):
 def todo_or_final(option):
     if option.startswith('t'):
         return 'todo'
-    elif options.startswith('f'):
+    elif option.startswith('f'):
         return 'final'
     raise argparse.ArgumentTypeError('type must be either todo or translation')
 
--- utils/sms_utils.py
+++ utils/sms_utils.py
@@ -4,20 +4,26 @@ import operator, collections, re, itertools
 
 class MessageRowBase(object):
 
-    def __init__(self,row,status,group,track,hiv,send_base,offset,english,swahili,luo,comment):
-        self.status, self.group, self.track, self.hiv = status, group, track, hiv
+    def __init__(self,row,status,group,track,hiv,send_base,offset,english,swahili,luo,**kwargs):
+        self.group, self.track, self.hiv = group, track, hiv
         self.send_base, self.offset = send_base, offset
-        self.english, self.swahili, self.luo, self.comment = map(clean_msg,(english,swahili,luo,comment))
-        self.new = ''
+        self.english, self.swahili, self.luo, = map(clean_msg,(english,swahili,luo))
 
+        self.comment = kwargs.get('comment','')
+        self.new = kwargs.get('new','')
         self.row = row[0].row
-        self.set_status()
-        self.group = self.group.replace('_','-')
-        self.configure_variables()
-        self.set_hiv_messaging()
 
-        if self.offset is None and self.status != 'comment':
-            self.offset = self.get_offset()
+        if any( [s is None for s in group,track,hiv,send_base,offset ] ):
+            status = '#'
+        self.set_status(status)
+
+        if self.status != 'comment':
+            self.group = self.group.replace('_','-')
+            self.configure_variables()
+            self.set_hiv_messaging()
+
+            if self.offset is None:
+                self.offset = self.get_offset()
 
 
     def description(self):
@@ -30,7 +36,7 @@ class MessageRowBase(object):
                 'condition':self.track,'comment':self.comment,'hiv_messaging':self.hiv,
                 'english':self.english if self.english != '' else self.new,
                 'swahili':self.swahili,'luo':self.luo,
-                'todo':self.status == 'todo'}
+                'todo':self.is_todo()}
 
     def get_offset(self):
         if self.send_base == 'signup':
@@ -62,20 +68,17 @@ class MessageRowBase(object):
         has_offset = self.offset is not None
         return group_valid and has_offset
 
-    def set_status(self):
-        if self.status is None:
-            self.status = 'clean'
-        elif self.status.lower() == 'x':
-            self.status =  'done'
-        elif self.status == '!':
-            self.status = 'todo'
-        elif self.status == '#':
-            self.status = 'comment'
+    def set_status(self,status):
+        if status is None:
+            self.status =  'clean'
         else:
-            self.status = 'clean'
+            self.status =  {'':'clean','x':'done','s':'swahili','l':'luo','!':'todo','#':'comment'}.get(status.lower()[0],'clean')
 
     def get_status_str(self):
-        return {'clean':'','done':'x','todo':'!','comment':'#'}.get(self.status,'')
+        return {'clean':'','done':'x','todo':'!','comment':'#','swahili':'s','luo':'l'}.get(self.status,'')
+
+    def is_todo(self):
+        return self.status in ['todo','swahili','luo']
 
     def set_hiv_messaging(self):
         if isinstance(self.hiv,basestring):
@@ -96,16 +99,29 @@ class MessageRowBase(object):
             for needle,string in variables:
                 self.english = self.english.replace(needle,string)
 
-        if self.english.startswith('{name}'):
-            if not self.swahili.startswith('{name}'):
-                self.swahili = '{name} huyu ni {nurse} kutoka {clinic} ' + self.swahili
-            if not self.luo.startswith('{name}'):
-                self.luo = '{name}, mae en {nurse} mawuok {clinic} ' + self.luo
+        if self.english.startswith('{name}, this is {nurse} from {clinic}'):
+            if not self.swahili.startswith('{name},'):
+                self.swahili = '{name}, huyu ni {nurse} kutoka {clinic}. ' + self.swahili
+            if not self.luo.startswith('{name},'):
+                self.luo = '{name}, mae en {nurse} mawuok {clinic}.' + self.luo
 
     def __repr__(self):
         return self.description()
 
     def get_translation_row(self,language):
+        return (
+            '!' if self.is_todo() and self.status != language else '',
+            self.group,
+            self.track,
+            self.get_hiv_messaging_str(),
+            self.send_base,
+            self.offset,
+            self.english,
+            self.new if self.status != language else '',
+            self.swahili if language == 'swahili' else self.luo
+        )
+
+    def get_final_row(self):
         return (
             self.get_status_str(),
             self.group,
@@ -114,8 +130,9 @@ class MessageRowBase(object):
             self.send_base,
             self.offset,
             self.english,
-            self.new,
-            self.swahili if language == 'swahili' else self.luo
+            self.swahili,
+            self.luo,
+            self.comment
         )
 
     @classmethod
@@ -130,12 +147,11 @@ class FinalRow(MessageRowBase):
     def __init__(self,row):
         status, group, track, hiv, send_base, offset, english, swahili, luo, comment= \
             cell_values( *operator.itemgetter(0,1,2,3,4,5,6,7,8,9)(row))
-        super(FinalRow,self).__init__(row,status,group,track,hiv,send_base,offset,english,swahili,luo,comment)
+        super(FinalRow,self).__init__(row,status,group,track,hiv,send_base,offset,
+            english,swahili,luo,comment=comment)
 
         # If message is translated make old equal new
-        if self.status == 'done' and self.new != '':
-            self.english = self.new
-            self.new = ''
+        if self.status == 'done':
             self.status = 'clean'
 
 class MessageBankRow(MessageRowBase):
@@ -143,42 +159,27 @@ class MessageBankRow(MessageRowBase):
     def __init__(self,row,old_translations=None):
         status, group, track, hiv, send_base, offset, english, comment= \
             cell_values( *operator.itemgetter(0,1,2,3,4,5,6,8)(row))
-        swahili,luo = '',''
-        super(MessageBankRow,self).__init__(row,status,group,track,hiv,send_base,offset,english,swahili,luo,comment)
+        swahili , luo = '' , ''
+        super(MessageBankRow,self).__init__(row,status,group,track,hiv,send_base,offset,
+            english,swahili,luo,comment=comment)
 
-        if old_translations is not None:
-            old = old_translations.get(self.description())
-            if self.to_translate(old):
-                self.new = self.english
-                self.english = old.english if old is not None else ''
-                self.swahili = old.swahili if old is not None else ''
-                self.luo = old.luo if old is not None else ''
-                self.status = 'todo'
+        if old_translations is None:
+            old_translations = {}
+        old = old_translations.get(self.description())
+
+        self.swahili = old.swahili if old is not None else self.swahili
+        self.luo = old.luo if old is not None else self.luo
+
+        if self.to_translate(old):
+            self.new = self.english
+            self.english = old.english if old is not None else ''
+            self.status = old.status if old is not None and old.is_todo() else 'todo'
 
     def to_translate(self,old):
-        if old is None or old.english != self.english or \
-           old.status == 'todo' or self.status == 'todo' or \
-           old.swahili == '' or old.luo == '':
+        if old is None or \
+           (old.english != self.english and self.status != 'done') or \
+           old.is_todo() or old.swahili == '' or old.luo == '':
             return True
-
-    def make_translation(self,old_translations):
-        old = old_translations.get(self.description())
-        old_english = old.english if old is not None else ''
-        new_english = self.english if self.to_translate(old) else ''
-        return (self.get_to_translate_str(old),self.group, self.track, 'yes' if self.hiv else 'no', self.send_base,
-            self.offset, old_english, new_english,
-            old.swahili if old is not None else '',
-            old.luo if old is not None else '',
-            self.comment)
-
-    def add_translation(self,ws,old_translations):
-        if self.is_valid():
-            row = self.make_translation(old_translations)
-            ws.append(row)
-            last = ws.rows[-1]
-            last[6].alignment, last[7].alignment, last[8].alignment, last[9].alignment = WRAP_TEXT, WRAP_TEXT, WRAP_TEXT, WRAP_TEXT
-            return 1 if row[0] == '!' else 0
-        return 0
 
 class TranslationRow(MessageRowBase):
 
@@ -186,13 +187,31 @@ class TranslationRow(MessageRowBase):
     def header(cls,language):
         return ('Todo','Group','Track','HIV','Base','Offset','Old','New',language)
 
+    def __init__(self,row,language_name):
+        status, group, track, hiv, send_base, offset, english, new, language = \
+            cell_values( *operator.itemgetter(0,1,2,3,4,5,6,7,8)(row))
+
+        language_name = language_name.lower()[0]
+        if language_name == 's':
+            swahili = language
+            luo = ''
+        elif language_name == 'l':
+            swahili = ''
+            luo = language
+
+        super(TranslationRow,self).__init__(row,status,group,track,hiv,send_base,offset,
+            english,swahili,luo,new=new)
+
 
 ########################################
 # Utility Functions
 ########################################
 
-def parse_messages(rows,cls):
-    return filter(lambda r: r.is_valid(), [cls(r) for r in rows] )
+def parse_messages(ws,cls,**kwargs):
+    for row in ws.rows:
+        msg = cls(row,**kwargs)
+        if msg.is_valid():
+            yield msg
 
 def read_sms_bank(bank,old=None,*args):
     return filter(lambda x: x.is_valid(), [
@@ -202,9 +221,9 @@ def read_sms_bank(bank,old=None,*args):
         )
     ])
 
-def message_dict(ws,cls):
+def message_dict(ws,cls,**kwargs):
     messages = collections.OrderedDict()
-    for msg in parse_messages(ws,cls):
+    for msg in parse_messages(ws,cls,**kwargs):
         messages[msg.description()] = msg
     return messages
 
