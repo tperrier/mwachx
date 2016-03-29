2af362ce0232c939e8fd24400df8000090e4cb7d
Trevor <t.perrier@gmail.com>
2016-03-28 21:39:28 -0700

    fix(cron) edd filter status uses pregnant instead of edd

--- utils/management/commands/command_utils.py
+++ utils/management/commands/command_utils.py
@@ -9,7 +9,7 @@ def set_edd_calls(email_body):
     email_body.append( "***** Set EDD Calls *****\n" )
 
     yesterday = datetime.date.today() - datetime.timedelta(days=1)
-    edd_today = cont.Contact.objects.filter(due_date=yesterday,status='edd',delivery_date__isnull=True)
+    edd_today = cont.Contact.objects.filter(due_date=yesterday,status='pregnant',delivery_date__isnull=True)
 
     email_body.append( "Found {} post edd participants on {}".format(len(edd_today),yesterday) )
 
