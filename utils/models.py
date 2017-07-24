398bd0e3c0fc49a44c81ca86ee251a2d11b32401
Cory Zue <czue@dimagi.com>
2017-07-24 13:46:15 -0400

    doc strings and whitespace

--- utils/models.py
+++ utils/models.py
@@ -2,25 +2,32 @@
 
 # Django Imports
 from django.db import models
-from django.core.exceptions import ObjectDoesNotExist, FieldError
+from django.core.exceptions import ObjectDoesNotExist
 from django.utils import timezone
 # Create your models here.
 
-class TimeStampedModel(models.Model):
 
-    #The date and time this message was created or modified
-    created = models.DateTimeField(default=timezone.now,editable=False)
+class TimeStampedModel(models.Model):
+    """
+    An abstract base class for storing created/modified metadata with a model
+    """
+    # The date and time this message was created or modified
+    created = models.DateTimeField(default=timezone.now, editable=False)
     modified = models.DateTimeField(auto_now=True)
 
-    def created_str(self,format='%Y-%m-%d %H:%M'):
+    def created_str(self, format='%Y-%m-%d %H:%M'):
         return self.created.strftime(format)
 
     class Meta:
         abstract = True
         ordering = ['-created']
 
-class BaseQuerySet(models.QuerySet):
 
+class BaseQuerySet(models.QuerySet):
+    """
+    A simple query set that adds some utility functions for getting objects
+    with default values.
+    """
     def get_or_none(self,**kwargs):
         return self.get_or_default(None,**kwargs)
 
@@ -29,6 +36,7 @@ class BaseQuerySet(models.QuerySet):
             return self.get(**kwargs)
         except ObjectDoesNotExist:
             return default
+
 
 class ForUserQuerySet(BaseQuerySet):
 
