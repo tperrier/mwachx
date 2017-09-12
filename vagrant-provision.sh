0f52bdd84bfb681a4acb7589086a9b800846753d
Ben Smith <bens23@uw.edu>
2017-09-12 16:24:23 -0700

    remove downloaded python and javascript libraries before downloading new ones

--- vagrant-provision.sh
+++ vagrant-provision.sh
@@ -9,6 +9,20 @@ npm install --global gulp-cli bower
 
 cd $MWACH_SOURCE_DIR
 
+if [ -d mwachx-virtualenv ]; then
+	rm -rf mwachx-virtualenv/
+fi
+
+if [ -d node_modules ]; then
+	rm -rf node_modules/
+fi
+
+if [ -d mwach/static/bower_components ]; then
+	rm -rf mwach/static/bower_components/
+fi
+
+find . -name "*.pyc" | xargs rm
+
 # Windows doesn't support symlinks, so virtualbox/vagrant shared folders on a 
 # Windows host does not, use --no-bin-links
 bower install --allow-root
