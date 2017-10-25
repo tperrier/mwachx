05ba46d525e1e8a38eef98fa399dbc735ebf8125
Trevor <t.perrier@gmail.com>
2017-10-25 16:16:24 -0700

    fix(AT) make at_status use new AT dump format

--- utils/management/commands/at_status.py
+++ utils/management/commands/at_status.py
@@ -15,158 +15,129 @@ from django.db import models , transaction
 import contacts.models as cont
 
 class Command(BaseCommand):
+    """ Command for checking AT Status from dump """
 
-    help = "manage success/sent time from AT message dump"
+    help = "Check/Update AT Status From Dump"
 
-    def add_arguments(self,parser):
+    def add_arguments(self, parser):
+        parser.add_argument('-f','--file',default='ignore/at_ids.csv',help='csv file of AT IDs')
+        subparsers = parser.add_subparsers(help='AT status commands')
 
-        parser.add_argument('-d','--dir',default='ignore/at_ids',help='default base dir')
-        parser.formatter_class = argparse.ArgumentDefaultsHelpFormatter
+        stats_parser = subparsers.add_parser('stats', cmd=parser.cmd, help='print default stats')
+        stats_parser.set_defaults(action='get_stats')
+        stats_parser.formatter_class = argparse.ArgumentDefaultsHelpFormatter
 
-        subparsers = parser.add_subparsers(help='at status actions')
+        diff_parser = subparsers.add_parser('diff', cmd=parser.cmd, help='make a csv of the different status messages')
+        diff_parser.set_defaults(action='make_diff_csv')
+        diff_parser.formatter_class = argparse.ArgumentDefaultsHelpFormatter
 
-        # The cmd argument is required for django.core.management.base.CommandParser
-        find_parser = subparsers.add_parser('find',cmd=parser.cmd,help='find AT IDs')
-        find_parser.set_defaults(action='find_at_ids')
-        find_parser.formatter_class = argparse.ArgumentDefaultsHelpFormatter
-
-        zeros_parser = subparsers.add_parser('zeros',cmd=parser.cmd,help="find AT IDs for zeros")
-        zeros_parser.set_defaults(action='find_zero_ids')
-        zeros_parser.formatter_class = argparse.ArgumentDefaultsHelpFormatter
-
-        check_parser = subparsers.add_parser('check',cmd=parser.cmd,help='check csv files')
-        check_parser.add_argument('input_csv',nargs='?',default='at_merged_list.csv',help='input csv file to check')
-        check_parser.set_defaults(action='check_csv')
-        check_parser.formatter_class = argparse.ArgumentDefaultsHelpFormatter
-
-        current_parser = subparsers.add_parser('current',cmd=parser.cmd,help='current csv files')
-        current_parser.set_defaults(action='current_status')
-        current_parser.formatter_class = argparse.ArgumentDefaultsHelpFormatter
-
-        make_parser = subparsers.add_parser('make',cmd=parser.cmd,help='make final update list')
-        make_parser.add_argument('input_csv',nargs='?',default='at_merged_list.csv',help='input csv file to check')
-        make_parser.set_defaults(action='make_update_list')
-        make_parser.formatter_class = argparse.ArgumentDefaultsHelpFormatter
-
-        update_parser = subparsers.add_parser('update',cmd=parser.cmd,help='update database')
-        update_parser.add_argument('input_csv',nargs='?',default='at_update_ids.csv',help='input csv file to check')
-        update_parser.add_argument('--live-run',action='store_true',default=False,help='make updates')
-        update_parser.set_defaults(action='update_ids')
+        update_parser = subparsers.add_parser('update', cmd=parser.cmd, help='update different mxy messages')
+        update_parser.set_defaults(action='update_different')
         update_parser.formatter_class = argparse.ArgumentDefaultsHelpFormatter
+
+    def run_from_argv(self,argv):
+        """
+        Provide a default command (stats)
+        """
+        print argv
+        if len(argv) == 2:
+            argv.append('stats')
+        super(Command,self).run_from_argv(argv)
 
     def handle(self,*args,**options):
 
-        print options
         self.options = options
-        getattr(self,options['action'])()
+        getattr(self, options['action'])()
 
-    def find_at_ids(self):
+    def get_stats(self):
+        """ Print basic stats about the at_ids.csv file """
 
-        self.print_header( "Finding AT Ids" )
+        reader = csv_row_maker(self.options['file'])
 
-        self.stdout.write( self.style.ERROR( 'Exiting' ) )
-        return
+        # Setup loop counter variables
+        min_date , max_date = None , None
+        sent_to , costs , statuses = co.Counter() , co.Counter() , co.Counter()
+        found , same = co.Counter() , co.Counter()
+        msg_ids = {msg.external_id:msg for msg in cont.Message.objects.filter(is_outgoing=True)}
 
-        at_msg_ids = csv.writer(open("ignore/at_msg_ids.csv",'w'))
-        at_msg_ids.writerow( ('date','to','identity','at_id','at_status','msg_status','msg') )
+        # Calculate stats for each row in at_id
+        for row in reader:
+            if max_date is None or row.date > max_date:
+                max_date = row.date
+            if min_date is None or row.date < min_date:
+                min_date = row.date
 
-        at_msg_ids_2 = csv.writer(open('ignore/at_msg_ids_2.csv','w'))
-        at_msg_ids_2.writerow( ('date','to','identity','at_id','status','msg_status','msg') )
+            sent_to[row.to] += 1
+            costs[row.cost] += 1
+            statuses[row.status] += 1
 
-        at_msg_ids_0 = csv.writer(open('ignore/at_msg_ids_0.csv','w'))
-        at_msg_ids_0.writerow( ('date','to','status','msg') )
-
-        at_csv = csv.reader(open("ignore/2017_01_31_at_dump.csv"))
-
-        # Make header row tuple
-        HeaderRow = co.namedtuple( "HeaderRow",next(at_csv) )
-        row_maker = row_factory_maker( HeaderRow )
-
-        for row in it.imap(row_maker, at_csv ):
-            msgs = find_msg_match(row)
-
-            if msgs.count() == 1:
-                msg = msgs.first()
-                at_msg_ids.writerow( (
-                    row.date,
-                    row.to,
-                    msg.connection.identity,
-                    msg.external_id,
-                    row.status,
-                    msg.external_status,
-                    row.msg
-                ) )
-            elif msgs.count() > 1:
-                for msg in msgs:
-                    at_msg_ids_2.writerow( (
-                        row.date,
-                        row.to,
-                        msg.connection.identity,
-                        msg.external_id,
-                        row.status,
-                        msg.external_status,
-                        row.msg
-                    ) )
+            if row.id in msg_ids:
+                found['yes'] += 1
+                if row.status == msg_ids[row.id].external_status:
+                    same['yes'] += 1
+                else:
+                    same['no'] += 1
             else:
-                at_msg_ids_0.writerow( (row.date, row.to, row.status, row.msg) )
+                found['no'] += 1
 
-    def check_csv(self):
+        self.stdout.write( "First Message: {}\nLast Message: {}".format(min_date,max_date) )
 
-        at_csv = csv_row_maker( self.dir_fp(self.options['input_csv']) )
+        self.stdout.write( "Sent To Count: {} Total: {}".format(len(sent_to),sum(sent_to.values() )) )
+        for number , count in sent_to.most_common(10):
+            self.stdout.write( "\t{} - {}".format(number,count) )
 
-        counts = co.Counter( (row.at_status,row.msg_status) for row in at_csv )
-        for count in counts.items():
-            print count
-        self.stdout.write( self.style.SQL_KEYWORD( 'Total: {}'.format( sum(counts.values()) ) ) )
+        self.stdout.write( "\nStatuses Count: {} Total: {}".format(len(statuses),sum(statuses.values() )) )
+        for status , count in statuses.most_common():
+            self.stdout.write( "\t{} - {}".format(status,count) )
 
-    def current_status(self):
+        self.stdout.write( "\nCosts Count: {} Total: {}".format(len(costs),sum(costs.values() )) )
+        for cost , count in costs.most_common():
+            self.stdout.write( "\t{} - {}".format(cost,count) )
 
-        self.print_header( 'Current Status' )
+        self.stdout.write( "\nFound" )
+        for value , count in found.most_common():
+            self.stdout.write( "\t{} - {}".format(value,count) )
 
-        counts = cont.Message.objects.order_by().values('external_status').annotate(count=models.Count('external_status'))
+        self.stdout.write( "\nSame" )
+        for value , count in same.most_common():
+            self.stdout.write( "\t{} - {}".format(value,count) )
 
-        for count in counts:
-            self.stdout.write( "  Status: {0[external_status]:<20.16} Count: {0[count]}".format(count) )
+    def make_diff_csv(self):
 
-    def make_update_list(self):
+        reader = csv_row_maker(self.options['file'])
+        msg_ids = {msg.external_id:msg for msg in cont.Message.objects.filter(is_outgoing=True)}
 
-        at_csv = csv_row_maker( self.dif_fp(self.options['input_csv']) )
+        writer = csv.writer( open('ignore/at_diff.csv','w') )
 
-        at_final = csv.writer(open(self.dif_fp('at_update_ids.csv'),'w'))
-        at_final.writerow( ('date','at_id','at_status','msg_status') )
+        writer.writerow( ('type','id','at_date','at_status','mxy_status','mxy_date') )
 
-        for row in it.imap( row_maker , at_csv):
-            if row.status != row.msg_status:
-                at_final.writerow( (
-                    row.date,
-                    row.at_id,
-                    row.status,
-                    row.msg_status
-                ))
+        for row in reader:
 
-    def update_ids(self):
+            if row.id in msg_ids and msg_ids[row.id].external_status != row.status:
+                writer.writerow(
+                    ( 'D',row.id,row.date,
+                        row.status,msg_ids[row.id].external_status,
+                        msg_ids[row.id].created
+                    )
+                )
+            elif row.id not in msg_ids:
+                writer.writerow( ( 'M',row.id,row.date, row.status, row.text ) )
 
-        self.print_header( "Update: Live={}".format(self.options['live_run']) )
 
-        at_csv = csv_row_maker( self.dir_fp(self.options['input_csv']) )
+    def update_different(self):
 
-        scheduled , updated = 0 , 0
+        reader = csv_row_maker(self.options['file'])
+        msg_ids = {msg.external_id:msg for msg in cont.Message.objects.filter(is_outgoing=True)}
+
+        count = 0
         with transaction.atomic():
-            for row in at_csv:
-                if row.at_status != row.msg_status:
-                    scheduled += 1
-                    if self.options['live_run']:
-                        updated += cont.Message.objects.filter(external_id=row.at_id).update(external_status=row.at_status)
+            for row in reader:
 
-        self.stdout.write( self.style.WARNING( "Scheduled: {} Updated: {}".format(scheduled,updated) ) )
-
-    def print_header(self,header):
-        self.stdout.write( self.style.WARNING( '*' * 50 + '\n*' ), ending='' )
-        self.stdout.write( self.style.SQL_KEYWORD( '{:^48}'.format(header) ), ending='' )
-        self.stdout.write( self.style.WARNING( '*\n' + '*' * 50 ) )
-
-    def dir_fp(self,fp):
-        return os.path.join(self.options['dir'],fp)
+                if row.id in msg_ids and msg_ids[row.id].external_status != row.status:
+                    msg_ids[row.id].external_status = row.status
+                    msg_ids[row.id].save()
+                    count += 1
+        self.stdout.write( "Updated {} messages".format(count) )
 
 ########################################
 #  Utilities
@@ -174,7 +145,7 @@ class Command(BaseCommand):
 def csv_row_maker(fp):
     csv_fp = csv.reader(open(fp))
     # Make header row tuple
-    HeaderRow = co.namedtuple( "HeaderRow",next(csv_fp) )
+    HeaderRow = co.namedtuple( "CSVRow",next(csv_fp) )
     row_maker = row_factory_maker( HeaderRow )
     return it.imap( row_maker , csv_fp )
 
@@ -183,20 +154,8 @@ def row_factory_maker(row_cls):
         Convert the first column from String Date to datetime
     """
     def _row_factory(row):
-        try:
-            new_date = datetime.datetime.strptime(row[0],"%I:%M %p %B %d, %Y")
-            new_date -= datetime.timedelta(hours=3) # Convert from EAT to UCT
-        except ValueError as e:
-            new_date = datetime.datetime.strptime(row[0],"%Y-%m-%d %H:%M:%S+00:00")
+        new_date = datetime.datetime.strptime(row[0],"%m/%d/%y %I:%M %p")
+        new_date += datetime.timedelta(hours=10) # Convert from PST to UCT
         row[0] = timezone.make_aware(new_date,timezone.utc)
         return row_cls._make(row)
     return _row_factory
-
-def find_msg_match(row,with_text=True,td=1):
-    end_time = row.date + datetime.timedelta(minutes=td)
-    identity = "+%s" % row.to
-    msg_Q = models.Q(created__range=(row.date,end_time),connection__identity=identity)
-    if with_text is True:
-        msg_Q &= models.Q(text=row.msg.strip())
-    msgs = cont.Message.objects.filter(msg_Q)
-    return msgs
